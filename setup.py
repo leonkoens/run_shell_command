20f41e34a30f292ad64b618fbd65c9d510f507d3
Leon Koens <leon@kirra.nl>
2015-12-17 16:31:24 +0100

    Add files for pypi deployment

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+    name='run_shell_command',
+    packages=['run_shell_command'],
+    version='1.0',
+    description='Execute bash commands from python',
+    author='Leon Koens',
+    author_email='pypi@leonkoens.nl',
+    url='https://github.com/leonkoens/run_shell_command',
+    download_url='https://github.com/peterldowns/mypackage/tarball/0.1',
+    keywords=['shell', 'command', 'bash', 'subprocess'],
+    classifiers=[],
+)
